496b06d12212157d0e497c982ef7081e4f8e049c
Guliko Tevadze <guliko.tevzadze.1@gmail.com>
2024-12-23 00:17:33 +0400

    add navigation

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenuHandler = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+  return (<>
+    <h1 className={`${isMenuOpen ? `black` : `white`} logo`}>Spell Sips</h1>
+    <div className='navigation'>
+      <nav className={`${isMenuOpen ? `active` : ``}`}>
+        <ul>
+          <li>
+            <Link to="/products" onClick={toggleMenuHandler}>
+              All Products
+            </Link>
+          </li>
+          <li>
+            <Link to="/" onClick={toggleMenuHandler}>
+              HOME
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      {!isMenuOpen ? (
+        <button className="navigation-burger " onClick={toggleMenuHandler}>
+          <i className="fa-solid fa-bars"></i>
+        </button>
+      ) : (
+        <button className="navigation-burger " onClick={toggleMenuHandler}>
+          <i className="fa-solid fa-x"></i>
+        </button>
+      )}
+    </div>
+  </>
+  );
+}
--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,11 +1,12 @@
-import { Outlet } from "react-router-dom"
-
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Navigation from '../components/Navigation';
 export default function RootLayout() {
-  return <>
-  <Header />
-  <Outlet />
-  <Footer />
-  </>
+  return (
+    <>
+      <Navigation/>
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
